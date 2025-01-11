12bd39c5dda74dd36977b8bc43c752d4993dd571
Saurav Joshi <saurav.tech.work@gmail.com>
2025-01-11 18:51:22 +0530

    TEST | String Calculator | refactor similar tests for single element in numbers string into one test

--- stringCalculator/index.test.ts
+++ stringCalculator/index.test.ts
@@ -5,14 +5,8 @@ test("return 0 if numbers string is empty", () => {
   expect(sum("")).toBe(0);
 })
 
-test("return 1 if numbers string contains only one element which is 1", () => {
-  expect(sum("1")).toBe(1);
-})
-
-test("return 2 if numbers string contains only one element which is 2", () => {
-  expect(sum("2")).toBe(2);
-})
-
-test("return 24 if numbers string contains only one element which is 24", () => {
-  expect(sum("24")).toBe(24);
+test("if there is only one number in numbers string return the same number", () => {
+  expect(sum("1")).toBe(1)
+  expect(sum("2")).toBe(2)
+  expect(sum("24")).toBe(24)
 })
