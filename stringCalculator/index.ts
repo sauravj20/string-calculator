8a9cb68dcb83b26f20f6c50e24327fd45308166b
Saurav Joshi <saurav.tech.work@gmail.com>
2025-01-11 23:21:07 +0530

    REFACTOR | String Calculator | extracted method for finding out custom delimiter

--- stringCalculator/index.ts
+++ stringCalculator/index.ts
@@ -1,18 +1,22 @@
-function sum(numbers: string): number {
-  if (numbers === "") {
+function separateDelimiterAndNumbers(numbersList: string) {
+  if (numbersList[0] === '/' && numbersList[1] === '/') {
+    return {
+      DELIMITER: new RegExp(numbersList[2]),
+      numbers: numbersList.substring(4)
+    };
+  }
+  return {DELIMITER: /[,\n]/, numbers: numbersList};
+}
+
+export default function sum(numbersList: string): number {
+  if (numbersList === "") {
     return 0;
   }
 
-  let DELIMITER = /[,\n]/;
-  if (numbers[0] === '/' && numbers[1] === '/') {
-    DELIMITER = new RegExp(numbers[2]);
-    numbers = numbers.substring(4)
-  }
+  const {DELIMITER, numbers} = separateDelimiterAndNumbers(numbersList);
 
   return numbers
     .split(DELIMITER)
     .map(Number)
     .reduce((a, b) => a + b, 0);
 }
-
-export default sum;
