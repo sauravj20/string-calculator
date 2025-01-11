7570bc687ccdcaa411ab2148f929a0fa6fca6bbb
Saurav Joshi <saurav.tech.work@gmail.com>
2025-01-11 21:47:02 +0530

    TEST | String Calculator | tests for adding multiple numbers in input string

--- stringCalculator/index.test.ts
+++ stringCalculator/index.test.ts
@@ -18,3 +18,11 @@ test("when input is 1 and 2 separated by a ',' add them to return 3", () => {
 test("when input is 5 and 6 separated by a ',' add them to return 11", () => {
   expect(sum("5,6")).toBe(11)
 })
+
+test("when input has 3 numbers separated by ',' add all of them to return the sum", () => {
+  expect(sum("1,2,3")).toBe(6);
+})
+
+test("when input has 4 numbers separated by ',' add all of them to return the sum", () => {
+  expect(sum("5,8,3,10")).toBe(26);
+})
